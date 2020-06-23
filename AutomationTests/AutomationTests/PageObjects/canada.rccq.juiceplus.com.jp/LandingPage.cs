0f20b8e0265452e5b3810f9a81852bf6e4686226
Jaron Crocker <jcrocker@JuicePlus.com>
2020-06-23 14:21:37 -0500

    Added juice plus legacy site test cases. Added Extent Reports.

---
+++ AutomationTests/AutomationTests/PageObjects/canada.rccq.juiceplus.com.jp/LandingPage.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTests.PageObjects.canada.rccq.juiceplus.com.jp
+{
+    class LandingPage
+    {
+        [OpenQA.Selenium.Support.PageObjects.FindsBy(How = How.Id, Using = "buy-page-nav-item")]
+        public IWebElement BuyNavItem { get; set; }
+        
+    }
+}
---
+++ AutomationTests/AutomationTests/PageObjects/canada.rccq.juiceplus.com.jp/OrchardandGardenBlendscs.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTests.PageObjects.canada.rccq.juiceplus.com.jp
+{
+    class OrchardandGardenBlendscs
+    {
+        [OpenQA.Selenium.Support.PageObjects.FindsBy(How = How.XPath, Using = "//div[@class='buttons hidden-xs col-sm-12']/a[.='Add to Cart']")]
+        public IWebElement AddToCartButton { get; set; }
+
+        [OpenQA.Selenium.Support.PageObjects.FindsBy(How = How.XPath, Using = "//a[.='Shopping Cart']")]
+        public IWebElement ShoppingCartIcon { get; set; }
+
+        [OpenQA.Selenium.Support.PageObjects.FindsBy(How = How.XPath, Using = "//div[@class='checkout-container']/a[.='Check Out']")]
+        public IWebElement ShoppingCartIconCheckoutButton { get; set; }
+        
+    }
+}
