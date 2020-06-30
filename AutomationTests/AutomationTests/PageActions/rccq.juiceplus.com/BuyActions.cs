9e5211533243650f3bbe8389d5d454311eef0792
Jaron Crocker <jcrocker@JuicePlus.com>
2020-06-30 15:20:26 -0500

    Added Juice Plus Store Legacy test cases.

---
+++ AutomationTests/AutomationTests/PageActions/rccq.juiceplus.com/BuyActions.cs
@@ -0,0 +1,60 @@
+﻿using AutomationTests.ConfigElements;
+using AutomationTests.PageObjects.juiceplus.com;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AutomationTests.PageActions.rccq.juiceplus.com.BuyActions
+{
+    class BuyActions
+    {
+        public static void ViewProduct(String product)
+        {
+            BuyPage BuyPage = new BuyPage();
+
+            if (product.Equals("FruitVegetableBerryBlendCapsules"))
+            {
+                BuyPage.FruitVegetableBerryBlendCapsules.Click();
+            }
+            else if (product.Equals("PremiumPackCapsules"))
+            {
+                BuyPage.PremiumPackCapsules.Click();
+            }
+            else
+            {
+                BuyPage.FruitVegetableBerryBlendCapsules.Click();
+            }
+
+        }
+        public static void AddToCart(int quantity)
+        {
+            BuyPage BuyPage = new BuyPage();
+            BuyPage.Quantity.Clear();
+            BuyPage.Quantity.SendKeys("" + quantity);
+            BuyPage.AddToCart.Click();
+            Thread.Sleep(1000);
+        }
+
+        public static void SelectPaymentType(String paymentType)
+        {
+            BuyPage BuyPage = new BuyPage();
+
+            if (paymentType.Equals("Monthly4x"))
+            {
+                BuyPage.PlanType4X.Click();
+            }
+            else if (paymentType.Equals("FullPayment"))
+            {
+                BuyPage.PlanTypeFullPayment.Click();
+            }
+        }
+    }
+}
---
+++ AutomationTests/AutomationTests/Tests/Juice Plus Store/CreateARO.cs
@@ -0,0 +1,92 @@
+﻿using AutomationTests.ConfigElements;
+using AutomationTests.PageActions.rccq.juiceplus.com;
+using AutomationTests.PageActions.rccq.juiceplus.com.BuyActions;
+using AutomationTests.PageActions.rccq.juiceplus.com.CartActions;
+using AutomationTests.PageActions.rccq.juiceplus.com.CheckoutActions;
+using AutomationTests.PageActions.rccq.juiceplus.com.NavigationsActions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutomationTests.juiceplus.com
+{
+    class CreateAROTest
+    {
+        NavigationActions NavigationActions = new NavigationActions();
+        BuyActions BuyActions = new BuyActions();
+        CartActions CartActions = new CartActions();
+        CheckoutActions CheckoutActions = new CheckoutActions();
+
+        [Test, Category("LegacyRegression"), Description("Create ARO on US Juice Plus Store")]
+        public void CreateARO_JP_US()
+        {
+            Driver.WebDriver.Navigate().GoToUrl(Config.Config.JuicePlusStoreUrl_US_RC);
+            Thread.Sleep(1000);
+            NavigationActions.NavigateBuyPage();
+            Thread.Sleep(1000);
+            BuyActions.ViewProduct("FruitVegetableBerryBlendCapsules");
+            BuyActions.SelectPaymentType("FullPayment");
+            BuyActions.AddToCart(1);
+            NavigationActions.NavigateCartPage();
+            CartActions.CheckOut();
+            CheckoutActions.InputShippingAndBillingInfo("Test", "Tester", "9018503000", "test@testing.com", "140 Crescent DR", "38017", "0.COLLIERVILLE", "TENNESSEE", "US", true, false);
+            CheckoutActions.InputPaymentMethod("Visa", "4242424242424242", "02", "2022", "189");
+            CheckoutActions.CompletePurchase();
+            Thread.Sleep(1000);
+            Assert.IsTrue(Driver.WebDriver.PageSource.Contains("Your order has been received. You will receive a confirmation email shortly. Orders may ship separately."));
+        }
+
+        [Test, Category("LegacyRegression"), Description("Create ARO on CA Juice Plus Store")]
+        public void CreateARO_JP_CA()
+        {
+            Driver.WebDriver.Navigate().GoToUrl(Config.Config.JuicePlusStoreUrl_CA_RC);
+            NavigationActions.NavigateBuyPage();
+            BuyActions.ViewProduct("FruitVegetableBerryBlendCapsules");
+            BuyActions.SelectPaymentType("FullPayment");
+            BuyActions.AddToCart(1);
+            NavigationActions.NavigateCartPage();
+            CartActions.CheckOut();
+            CheckoutActions.InputShippingAndBillingInfo("Test", "Tester", "9018503000", "test@testing.com", "2785 Skymark Ave", "L4W 4Y3", "Ontario", "Ontario", "CA", true, false);
+            CheckoutActions.InputPaymentMethod("Visa", "4242424242424242", "02", "2022", "189");
+            CheckoutActions.CompletePurchase();
+            Thread.Sleep(1000);
+            Assert.IsTrue(Driver.WebDriver.PageSource.Contains("Your order has been received. You will receive a confirmation email shortly. Orders may ship separately."));
+        }
+
+        [Test, Category("LegacyRegression"), Description("Create ARO on AU Juice Plus Store")]
+        public void CreateARO_JP_AU()
+        {
+            Driver.WebDriver.Navigate().GoToUrl(Config.Config.JuicePlusStoreUrl_AU_RC);
+            NavigationActions.NavigateBuyPage();
+            BuyActions.ViewProduct("PremiumPackCapsules");
+            BuyActions.SelectPaymentType("FullPayment");
+            BuyActions.AddToCart(1);
+            NavigationActions.NavigateCartPage();
+            CartActions.CheckOut();
+            CheckoutActions.InputShippingAndBillingInfo("Test", "Tester", "9018503000", "test@testing.com", "14 Merewether St", "2291", "Merewether", "Queensland", "AU", true, false);
+            CheckoutActions.InputPaymentMethod("Visa", "4242424242424242", "02", "2022", "189");
+            CheckoutActions.CompletePurchase();
+            Thread.Sleep(1000);
+            Assert.IsTrue(Driver.WebDriver.PageSource.Contains("Your order has been received. You will receive a confirmation email shortly. Orders may ship separately."));
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Teardown();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            Driver.InitializeDriver();
+            Driver.WebDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+    }
+
+}
